fde61d833665a65b287df9dfb60e120f82bfbb98
Student User <student@workstation.lab.example.com>
2021-05-28 05:54:58 -0400

    changed blabla

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,4 +2,10 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "... A change is in the air!";
 print "blabla";
+print "der dreck hat vorher ned funktioniert! ich editiere das über die github-gui!";
+print "versuch3...";
+print "versuch4...";
+
+print "schauma was heute so passiert!";
+
 ?>
